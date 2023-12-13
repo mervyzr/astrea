d05ec65e9f912c38ece9e0abaaf965c0459883d5
mervyzr <mervin.yzr@hotmail.com>
2023-12-13 17:23:04 +0100

    Edited live plot feature

--- simulation.py
+++ simulation.py
@@ -50,30 +50,7 @@ def runSimulation(N, _config=config, _cfl=cfl, _gamma=gamma, _startPos=startPos,
     t = 0
 
     if livePlot:
-        plot_x = np.linspace(_startPos, _endPos, N)
-        plt.ion()
-        fig, ax = plt.subplots(nrows=2, ncols=2)
-        ax[0,0].set_ylabel(r"density $\rho$")
-        ax[0,1].set_ylabel(r"pressure $P$")
-        ax[1,0].set_ylabel(r"velocity $v_x$")
-        ax[1,1].set_ylabel(r"thermal energy $P/\rho$")
-        ax[0,1].yaxis.set_label_position("right")
-        ax[1,1].yaxis.set_label_position("right")
-        ax[0,1].yaxis.tick_right()
-        ax[1,1].yaxis.tick_right()
-        ax[0,0].set_xlim([_startPos, _endPos])
-        ax[0,1].set_xlim([_startPos, _endPos])
-        ax[1,0].set_xlim([_startPos, _endPos])
-        ax[1,1].set_xlim([_startPos, _endPos])
-        ax[0,0].grid(linestyle='--', linewidth=0.5)
-        ax[0,1].grid(linestyle='--', linewidth=0.5)
-        ax[1,0].grid(linestyle='--', linewidth=0.5)
-        ax[1,1].grid(linestyle='--', linewidth=0.5)
-
-        graphTL, = ax[0,0].plot(plot_x, plot_x, linewidth=2, color="blue")  # density
-        graphTR, = ax[0,1].plot(plot_x, plot_x, linewidth=2, color="red")  # pressure
-        graphBL, = ax[1,0].plot(plot_x, plot_x, linewidth=2, color="green")  # vx
-        graphBR, = ax[1,1].plot(plot_x, plot_x, linewidth=2, color="black")  # thermal energy
+        fig, ax, plots = plotter.initiateLivePlot(_startPos, _endPos, N)
 
     while t <= _tEnd:
         # Saves each instance of the system at time t
@@ -81,22 +58,7 @@ def runSimulation(N, _config=config, _cfl=cfl, _gamma=gamma, _startPos=startPos,
         simulation[t] = np.copy(tube)
 
         if livePlot:
-            graphTL.set_ydata(tube[:,0])  # density
-            graphTR.set_ydata(tube[:,4])  # pressure
-            graphBL.set_ydata(tube[:,1])  # vx
-            graphBR.set_ydata(tube[:,4]/tube[:,0])  # thermal energy
-            ax[0,0].relim()
-            ax[0,0].autoscale_view()
-            ax[0,1].relim()
-            ax[0,1].autoscale_view()
-            ax[1,0].relim()
-            ax[1,0].autoscale_view()
-            ax[1,1].relim()
-            ax[1,1].autoscale_view()
-            plt.suptitle(rf"Quantities $q$ against cell position $x$ at $t = {round(t,4)}$")
-            fig.text(0.5, 0.04, r"Cell position $x$", ha='center')
-            fig.canvas.draw()
-            fig.canvas.flush_events()
+            plotter.updatePlot(tube, t, fig, ax, plots)
 
         # Compute the numerical fluxes at each interface
         hydroTube = solver.GodunovSolver(domain, _config, _gamma)
