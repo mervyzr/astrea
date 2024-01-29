4aef79247fa192ca94a8473e9e45137de1c0a99b
Mervin Yap <mervin.yzr@hotmail.com>
2024-01-29 15:00:46 +0100

    Added settings.py file
    This file changes the settings for the simulation and contains minimal code, therefore it is included in .gitignore.

---
+++ settings.py
@@ -0,0 +1,15 @@
+
+##############################################################################
+
+config = "sod"
+cells = 5000
+cfl = .5
+gamma = 1.4
+solver = "ppm"
+
+runType = "single"
+livePlot = True
+
+saveFile = False
+snapshots = 1
+saveVideo = False
