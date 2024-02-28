d2e4297c1cf52e070c45bb352779a3d7429befee
mervyzr <mervin.yzr@hotmail.com>
2024-02-28 16:20:29 +0100

    feature: added slope flattener function

--- functions/fv.py
+++ functions/fv.py
@@ -38,7 +38,7 @@ def pointConvertConservative(tube, g):
     return np.c_[rhos, vecs, pressures]
 
 
-# Converting primitive variables w to conservative variables q through a higher-order approx.
+# Converting cell-averaged primitive variables w to cell-averaged conservative variables q through a higher-order approx.
 def convertPrimitive(tube, g, boundary, dem=24):
     wLs, wRs = makeBoundary(tube, boundary)
     w = tube - ((np.diff(wRs, axis=0) - np.diff(wLs, axis=0)) / dem)  # 2nd-order Taylor expansion (Laplacian)
@@ -46,7 +46,7 @@ def convertPrimitive(tube, g, boundary, dem=24):
     return pointConvertPrimitive(w, g) + ((np.diff(qRs, axis=0) - np.diff(qLs, axis=0)) / dem)
     
 
-# Converting conservative variables q to primitive variables w through a higher-order approx.
+# Converting cell-averaged conservative variables q to cell-averaged primitive variables w through a higher-order approx.
 def convertConservative(tube, g, boundary, dem=24):
     qLs, qRs = makeBoundary(tube, boundary)
     q = tube - ((np.diff(qRs, axis=0) - np.diff(qLs, axis=0)) / dem)  # 2nd-order Taylor expansion (Laplacian)
@@ -69,3 +69,29 @@ def makeFlux(tube, g):
     rhos, vecs, pressures = tube[:,0], tube[:,1:4], tube[:,4]
     return np.c_[rhos*vecs[:,0], rhos*(vecs[:,0]**2) + pressures, rhos*vecs[:,0]*vecs[:,1], rhos*vecs[:,0]*vecs[:,2],\
                     vecs[:,0] * ((.5*rhos*np.linalg.norm(vecs, axis=1)**2) + ((g*pressures)/(g-1)))]
+
+
+# Function that returns the coefficient of the slope flattener
+def getSlopeCoeff(tube, boundary, slope_determinants=[.75, .85, .33]):
+    z0, z1, delta = slope_determinants
+    domain = pointConvertConservative(tube)
+    arr, chi = np.ones(len(domain)), np.ones(len(domain))
+
+    wLs, wRs = makeBoundary(domain, boundary)
+    if boundary == "periodic":
+        wL2s, wR2s = np.concatenate(([wLs[-2]],wLs)), np.concatenate((wRs,[wRs[1]]))
+    else:
+        wL2s, wR2s = np.concatenate(([wLs[0]],wLs)), np.concatenate((wRs,[wRs[-1]]))
+
+    z = np.abs((wRs[:,4][1:] - wLs[:,4][:-1]) / (wR2s[:,4][2:] - wL2s[:,4][:-2]))  # define the linear function
+    eta = np.minimum(np.ones(len(z)), np.maximum(np.zeros(len(z)), 1 - ((z-z0)/(z1-z0))))  # limit the range between 0 and 1
+    criteria = (wLs[:,1][:-1] - wRs[:,1][1:] > 0) & (np.abs(wRs[:,4][1:] - wLs[:,4][:-1])/np.minimum(wRs[:,4][1:], wLs[:,4][:-1]) > delta)
+    
+    chi[criteria] = eta[criteria]
+    chiL, chiR = makeBoundary(chi, boundary)
+
+    signage = np.sign(wRs[:,4][1:] - wLs[:,4][:-1])
+    arr[signage < 0] = np.minimum(chi, chiR[1:])[signage < 0]
+    arr[signage > 0] = np.minimum(chi, chiL[:-1])[signage > 0]
+
+    return arr
