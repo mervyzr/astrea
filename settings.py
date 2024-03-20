03245a7e7d12e17cda0283706a3464570c6f6755
Mervin Yap <mervin.yzr@hotmail.com>
2024-03-20 22:06:06 +0100

    refactor: edited comments

--- settings.py
+++ settings.py
@@ -1,16 +1,27 @@
 
 ##############################################################################
 
-config = "sod"  # Sod, Sedov, Shu-Osher, sin wave, square wave, Toro1/2/3/4/5
+# Test configurations
+# : Sod, Sedov, Shu-Osher, sin wave, square wave, Toro1/2/3/4/5
+config = "sod"
+
+# Shock tube parameters
 cells = 200
 cfl = .5
 gamma = 1.4
-solver = "ppm"  # PCM, PLM, PPM
-timestep = "ssprk(5,4)"  # Euler, RK4, SSPRK(2,2), SSPRK(3,3), SSPRK(4,3), SSPRK(5,3), SSPRK(5,4)
 
-runType = "single"  # Single/Multiple
+# Numerical methods
+# : PCM, PLM, PPM
+solver = "ppm"
+# : Euler, RK4, SSPRK(2,2), SSPRK(3,3), SSPRK(4,3), SSPRK(5,3), SSPRK(5,4)
+timestep = "ssprk(5,4)"
+
+# Runtime parameters
+# : Single, Multiple
+runType = "single"
 livePlot = False
 
+# Media options
 savePlots = False
 snapshots = 1
 saveVideo = False
