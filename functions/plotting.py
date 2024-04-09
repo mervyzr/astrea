ee58a755c4a128ded04365a7ae12f49166fc7db2
mervyzr <mervin.yzr@hotmail.com>
2024-04-09 17:58:17 +0200

    feature: added analytical solution for sin-wave test

--- functions/plotting.py
+++ functions/plotting.py
@@ -131,37 +131,33 @@ def plotQuantities(f, configVariables, testVariables, savepath):
                 ax[1,1].plot(x, y4, linewidth=2, color="darkviolet")  # thermal energy
                 plt.suptitle(rf"Plot of quantities $q$ against cell position $x$ at $t \approx {round(indexes[-1][i],3)}$ ($N = {len(y1)}$)", fontsize=24)
 
-        # Adjust ylim for sin-wave test
-        if config.startswith("sin"):
+        # Adjust ylim and plot analytical solutions for Gaussian and sin-wave tests
+        if config.startswith("sin") or config.startswith("gaussian"):
             last_sim = f[list(f.keys())[-1]]
             first_config = last_sim[list(last_sim.keys())[0]][0]
-            initial_rho, initial_vx, initial_vy, initial_vz, initial_P, initial_Bx, initial_By, initial_Bz = first_config
-            vrange = np.linspace(initial_vx-.005, initial_vx+.005, 9)
-            Prange = np.linspace(initial_P-.005, initial_P+.005, 9)
-            ax[0,1].set_yticks(Prange)
-            ax[1,0].set_yticks(vrange)
-            ax[0,1].set_ylim([initial_P-.005, initial_P+.005])
-            ax[1,0].set_ylim([initial_vx-.005, initial_vx+.005])
-
-        # Add Gaussian analytical solution and adjust ylim
-        elif config.startswith("gauss"):
             initialConfig = testVariables['initialLeft']
             midpoint = (endPos+startPos)/2
-            gaussian = np.zeros((N, len(initialConfig)), dtype=np.float64)
-            gaussian[:] = initialConfig
-            gaussian[:,0] = 1e-3 + (1-1e-3) * np.exp(-(x-midpoint)**2/.01)
 
-            ax[0,0].plot(x, gaussian[:, 0], linewidth=1, color="black", linestyle="--", label="Analytical solution")
-            ax[0,1].plot(x, gaussian[:, 4], linewidth=1, color="black", linestyle="--", label="Analytical solution")
-            ax[1,0].plot(x, gaussian[:, 1], linewidth=1, color="black", linestyle="--", label="Analytical solution")
-            ax[1,1].plot(x, gaussian[:, 4]/gaussian[:, 0], linewidth=1, color="black", linestyle="--", label="Analytical solution")
+            analytical = np.zeros((N, len(initialConfig)), dtype=np.float64)
+            analytical[:] = initialConfig
+            if config.startswith("gaussian"):
+                analytical[:,0] = 1e-3 + (1-1e-3) * np.exp(-(x-midpoint)**2/.01)
+                Ptol = 5e-7
+            else:
+                analytical[:,0] = 1 + (.1 * np.sin(testVariables['freq']*np.pi*x))
+                Ptol = .005
 
-            Prange = np.linspace(initialConfig[4]-5e-7, initialConfig[4]+5e-7, 9)
+            Prange = np.linspace(initialConfig[4]-Ptol, initialConfig[4]+Ptol, 9)
             vrange = np.linspace(initialConfig[1]-.005, initialConfig[1]+.005, 9)
             ax[0,1].set_yticks(Prange)
             ax[1,0].set_yticks(vrange)
-            ax[0,1].set_ylim([initialConfig[4]-5e-7, initialConfig[4]+5e-7])
+            ax[0,1].set_ylim([initialConfig[4]-Ptol, initialConfig[4]+Ptol])
             ax[1,0].set_ylim([initialConfig[1]-.005, initialConfig[1]+.005])
+
+            ax[0,0].plot(x, analytical[:, 0], linewidth=1, color="black", linestyle="--", label="Analytical solution")
+            ax[0,1].plot(x, analytical[:, 4], linewidth=1, color="black", linestyle="--", label="Analytical solution")
+            ax[1,0].plot(x, analytical[:, 1], linewidth=1, color="black", linestyle="--", label="Analytical solution")
+            ax[1,1].plot(x, analytical[:, 4]/analytical[:, 0], linewidth=1, color="black", linestyle="--", label="Analytical solution")
 
         # Add Sod analytical solution, using the highest resolution and timing
         elif config == "sod":
@@ -177,7 +173,7 @@ def plotQuantities(f, configVariables, testVariables, savepath):
             pass
 
         fig.text(0.5, 0.04, r"Cell position $x$", fontsize=18, ha='center')
-        if len(f) != 1 or config == "sod" or config.startswith("gauss"):
+        if len(f) != 1 or config == "sod" or config.startswith("gauss") or config.startswith("sin"):
             handles, labels = plt.gca().get_legend_handles_labels()
             fig.legend(handles, labels, prop={'size': 16}, loc='upper right')
 
