f0485594a012bb3e6a77dd5923696734130d9af7
mervyzr <mervin.yzr@hotmail.com>
2024-04-09 15:34:12 +0200

    refactor: minor edits to print functions

--- setup.py
+++ setup.py
@@ -14,13 +14,13 @@ home = os.path.expanduser("~")
 print("Initialising setup..")
 
 if not os.path.exists(f"{currentdir}/settings.py"):
-    print(f"Copying settings file from default..")
     shutil.copy2(f"{currentdir}/functions/.default.py", f"{currentdir}/settings.py")
 
 if not os.path.isdir(f"{home}/.shock_venv"):
     print("Creating venv for simulation..")
     venv_dir = os.path.join(home, ".shock_venv")
     venv.create(venv_dir, with_pip=True)
+    print("Installing Python packages..")
     subprocess.run(["bin/pip", "install", "-q", "-r", os.path.abspath("requirements.txt")], cwd=venv_dir)
 
 print("Setup complete!")
