fix: averaging function for PPM (incomplete)

solvers.py
@@ -7,14 +7,17 @@ from functions import fv
 ##############################################################################
 
 # Solve the Riemann (flux) problem (Local Lax-Friedrichs; approximate Roe solver)
-def calculateRiemannFlux(solutions, gamma, solver, boundary):
+def calculateRiemannFlux(tube, solutions, gamma, solver, boundary):
     if solver in ["ppm", "parabolic", "p", "plm", "linear", "l"]:
         leftSolution, rightSolution = solutions
         leftInterface, rightInterface = fv.makeBoundary(leftSolution, boundary)[1:], fv.makeBoundary(rightSolution, boundary)[:-1]
 
         # Get the average of the solutions by integrating the interpolated values
         if solver in ["ppm", "parabolic", "p"]:
-            avg_wS = (leftSolution + rightSolution)/2  # [Colella & Woodward, 1984, eq. 1.4]
+            avg_wS = (leftSolution + rightSolution)/2
+            # [Colella & Woodward, 1984, eq. 1.4-1.5]
+            #q6 = np.copy(6 * (tube - .5 * (leftSolution+rightSolution)))
+            #avg_wS = leftSolution + .5*(rightSolution - leftSolution + .5*q6)  # Only for uniform grid
         else:
             avg_wS = (leftSolution + rightSolution)/2
 