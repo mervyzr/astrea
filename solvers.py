cc7f6dbc7e11622adadd6648dd9a62a2a2785c73
Mervin Yap <mervin.yzr@hotmail.com>
2024-04-12 08:18:57 +0200

    refactor: combined conditional statements

--- solvers.py
+++ solvers.py
@@ -14,26 +14,26 @@ def calculateRiemannFlux(solutions, gamma, solver, boundary):
 
         # Get the average of the solutions by integrating the interpolated values
         if solver in ["ppm", "parabolic", "p"]:
-            avg_wS = (leftSolution + rightSolution)/2  # [Colella & Woodward, 1984, eq. 1.4]
+            # [Colella & Woodward, 1984, eq. 1.4]
+            avg_wS = (leftSolution + rightSolution)/2
         else:
             avg_wS = (leftSolution + rightSolution)/2
+    else:
+        avg_wS = solutions
 
-        # Ideally, the 4th-order averaged fluxes should be computed from the face-averaged variables
-        # But because the simulation is only 1D, the "normal"-Laplacian (Taylor expansion) of the face-averaged states and fluxes are zero
-        # Thus, the face-averaged and face-centred values are the same (<w>_i+1/2 = w_i+1/2)
-        # Same for the averaged and centred fluxes (<F>_i+1/2 = F_i+1/2)
-        wS = fv.makeBoundary(avg_wS, boundary)
-        fS = fv.makeFlux(wS, gamma)
-        A = fv.makeJacobian(wS, gamma)
+    # Ideally, the 4th-order averaged fluxes should be computed from the face-averaged variables
+    # But because the simulation is only 1D, the "normal"-Laplacian (Taylor expansion) of the face-averaged states and fluxes are zero
+    # Thus, the face-averaged and face-centred values are the same (<w>_i+1/2 = w_i+1/2)
+    # Same for the averaged and centred fluxes (<F>_i+1/2 = F_i+1/2)
+    wS = fv.makeBoundary(avg_wS, boundary)
+    fS = fv.makeFlux(wS, gamma)
+    A = fv.makeJacobian(wS, gamma)
 
+    if solver in ["ppm", "parabolic", "p", "plm", "linear", "l"]:
         # The conversion can be pointwise conversion for the face-averaged values
         qLs, qRs = fv.pointConvertPrimitive(leftInterface, gamma), fv.pointConvertPrimitive(rightInterface, gamma)
         qDiff = (qLs - qRs).T
     else:
-        wS = fv.makeBoundary(solutions, boundary)
-        fS = fv.makeFlux(wS, gamma)
-        A = fv.makeJacobian(wS, gamma)
-
         qLs, qRs = fv.pointConvertPrimitive(wS[:-1], gamma), fv.pointConvertPrimitive(wS[1:], gamma)
         qDiff = (qRs - qLs).T
 
