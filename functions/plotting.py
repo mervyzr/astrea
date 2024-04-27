1d74a6c0c8f358ae730faeed559d709eb94c98e8
Mervin Yap <mervin.yzr@hotmail.com>
2024-04-28 00:51:06 +0200

    refactor: loop for plot setups

--- functions/plotting.py
+++ functions/plotting.py
@@ -21,14 +21,17 @@ from functions import generic, analytic
 plt.style.use("default")
 beautify = False
 
+
+plotIndexes = [[0,0], [0,1], [1,0], [1,1]]
+labels = [r"Density $\rho$", r"Pressure $P$", r"Velocity $v_x$", r"Thermal energy $\frac{P}{\rho}$"]
+colours = ["blue", "red", "green", "darkviolet"]
+
+
 # Initiate the live plot feature
 def initiateLivePlot(startPos, endPos, N):
     plt.ion()
     fig, ax = plt.subplots(nrows=2, ncols=2)
 
-    graphs = []
-    labels = [r"Density $\rho$", r"Pressure $P$", r"Velocity $v_x$", r"Thermal energy $\frac{P}{\rho}$"]
-    colours = ["blue", "red", "green", "darkviolet"]
     plot_x = np.linspace(startPos, endPos, N)
 
     ax[0,1].yaxis.set_label_position("right")
@@ -37,7 +40,8 @@ def initiateLivePlot(startPos, endPos, N):
     ax[0,1].yaxis.tick_right()
     ax[1,1].yaxis.tick_right()
 
-    for index, (_i,_j) in enumerate([[0,0], [0,1], [1,0], [1,1]]):
+    graphs = []
+    for index, (_i,_j) in enumerate(plotIndexes):
         ax[_i,_j].set_ylabel(labels[index])
         ax[_i,_j].set_xlim([startPos, endPos])
         ax[_i,_j].grid(linestyle='--', linewidth=0.5)
@@ -56,7 +60,7 @@ def updatePlot(arr, t, fig, ax, graphs):
     graphBL.set_ydata(arr[:,1])  # vx
     graphBR.set_ydata(arr[:,4]/arr[:,0])  # thermal energy
 
-    for _i, _j in [[0,0], [0,1], [1,0], [1,1]]:
+    for _i, _j in plotIndexes:
         ax[_i,_j].relim()
         ax[_i,_j].autoscale_view()
 
@@ -87,18 +91,11 @@ def plotQuantities(f, configVariables, testVariables, savepath):
     for i in range(len(indexes[-1])):
         fig, ax = plt.subplots(nrows=2, ncols=2, figsize=[21, 10])
 
-        ax[0,0].set_ylabel(r"Density $\rho$", fontsize=18)
-        ax[0,1].set_ylabel(r"Pressure $P$", fontsize=18)
-        ax[1,0].set_ylabel(r"Velocity $v_x$", fontsize=18)
-        ax[1,1].set_ylabel(r"Thermal energy $\frac{P}{\rho}$", fontsize=18)
-        ax[0,0].set_xlim([startPos, endPos])
-        ax[0,1].set_xlim([startPos, endPos])
-        ax[1,0].set_xlim([startPos, endPos])
-        ax[1,1].set_xlim([startPos, endPos])
-        ax[0,0].grid(linestyle='--', linewidth=0.5)
-        ax[0,1].grid(linestyle='--', linewidth=0.5)
-        ax[1,0].grid(linestyle='--', linewidth=0.5)
-        ax[1,1].grid(linestyle='--', linewidth=0.5)
+        # Set up figure
+        for index, (_i,_j) in enumerate(plotIndexes):
+            ax[_i,_j].set_ylabel(labels[index], fontsize=18)
+            ax[_i,_j].set_xlim([startPos, endPos])
+            ax[_i,_j].grid(linestyle="--", linewidth=0.5)
 
         # Plot each simulation at the i-th timing
         for j, N in enumerate(nList):
@@ -109,10 +106,9 @@ def plotQuantities(f, configVariables, testVariables, savepath):
             y4 = y2/y1                       # thermal energy
             x = np.linspace(startPos, endPos, N)
             y_data = [y1, y2, y3, y4]
-            colours = ["blue", "red", "green", "darkviolet"]
 
             # density, pressure, vx, thermal energy
-            for index, (_i,_j) in enumerate([[0,0], [0,1], [1,0], [1,1]]):
+            for index, (_i,_j) in enumerate(plotIndexes):
                 if len(f) != 1:
                     ax[_i,_j].plot(x, y_data[index], linewidth=2, label=f"N = {N}")
                     plt.suptitle(rf"Primitive variables $\vec{{w}}$ against cell position $x$ at $t \approx {round(indexes[-1][i],3)}$", fontsize=24)
@@ -146,19 +142,18 @@ def plotQuantities(f, configVariables, testVariables, savepath):
             ax[0,1].set_ylim([initialConfig[4]-Ptol, initialConfig[4]+Ptol])
             ax[1,0].set_ylim([initialConfig[1]-.005, initialConfig[1]+.005])
 
-            ax[0,0].plot(x, analytical[:, 0], linewidth=1, color="black", linestyle="--", label="Analytical solution")
-            ax[0,1].plot(x, analytical[:, 4], linewidth=1, color="black", linestyle="--", label="Analytical solution")
-            ax[1,0].plot(x, analytical[:, 1], linewidth=1, color="black", linestyle="--", label="Analytical solution")
-            ax[1,1].plot(x, analytical[:, 4]/analytical[:, 0], linewidth=1, color="black", linestyle="--", label="Analytical solution")
+            y_theo = [analytical[:, 0], analytical[:, 4], analytical[:, 1], analytical[:, 4]/analytical[:, 0]]
+            for index, (_i,_j) in enumerate(plotIndexes):
+                ax[_i,_j].plot(x, y_theo[index], linewidth=1, color="black", linestyle="--", label="Analytical solution")
 
         # Add Sod analytical solution, using the highest resolution and timing
         elif config == "sod":
             tube, _t = f[str(nList[-1])][str(indexes[-1][i])], indexes[-1][i]
             Sod = analytic.calculateSodAnalytical(tube, _t,  gamma, startPos, endPos, shockPos)
-            ax[0,0].plot(x, Sod[:, 0], linewidth=1, color="black", linestyle="--", label="Analytical solution")
-            ax[0,1].plot(x, Sod[:, 4], linewidth=1, color="black", linestyle="--", label="Analytical solution")
-            ax[1,0].plot(x, Sod[:, 1], linewidth=1, color="black", linestyle="--", label="Analytical solution")
-            ax[1,1].plot(x, Sod[:, 4]/Sod[:, 0], linewidth=1, color="black", linestyle="--", label="Analytical solution")
+
+            y_theo = [Sod[:, 0], Sod[:, 4], Sod[:, 1], Sod[:, 4]/Sod[:, 0]]
+            for index, (_i,_j) in enumerate(plotIndexes):
+                ax[_i,_j].plot(x, y_theo[index], linewidth=1, color="black", linestyle="--", label="Analytical solution")
 
         # Add Sedov analytical solution, using the highest resolution and timing
         elif config == "sedov":
@@ -186,15 +181,11 @@ def plotSolutionErrors(f, configVariables, testVariables, savepath):
     nList.sort()
 
     fig, ax = plt.subplots(nrows=2, ncols=2, figsize=[21, 10])
+    errorLabels = [r"Density $\log{(\epsilon_\nu(\rho))}$", r"Pressure $\log{(\epsilon_\nu(P))}$", r"Velocity $\log{(\epsilon_\nu(v_x))}$", r"Thermal energy $\log{(\epsilon_\nu(\frac{P}{\rho}))}$"]
 
-    ax[0,0].set_ylabel(r"Density $\log{(\epsilon_\nu(\rho))}$", fontsize=18)  # density
-    ax[0,1].set_ylabel(r"Pressure $\log{(\epsilon_\nu(P))}$", fontsize=18)  # pressure
-    ax[1,0].set_ylabel(r"Velocity $\log{(\epsilon_\nu(v_x))}$", fontsize=18)  # vx
-    ax[1,1].set_ylabel(r"Thermal energy $\log{(\epsilon_\nu(\frac{P}{\rho}))}$", fontsize=18)  # thermal energy
-    ax[0,0].grid(linestyle='--', linewidth=0.5)
-    ax[0,1].grid(linestyle='--', linewidth=0.5)
-    ax[1,0].grid(linestyle='--', linewidth=0.5)
-    ax[1,1].grid(linestyle='--', linewidth=0.5)
+    for index, (_i,_j) in enumerate(plotIndexes):
+        ax[_i,_j].set_ylabel(errorLabels[index], fontsize=18)
+        ax[_i,_j].grid(linestyle="--", linewidth=0.5)
 
     x, y1, y2, y3, y4 = [], [], [], [], []
     for N in nList:
@@ -205,21 +196,12 @@ def plotSolutionErrors(f, configVariables, testVariables, savepath):
         y3.append(solutionErrors[1])  # vx
         y4.append(solutionErrors[-1])  # thermal energy
     x, y1, y2, y3, y4 = np.asarray(x), np.asarray(y1), np.asarray(y2), np.asarray(y3), np.asarray(y4)
+    y_data = [y1, y2, y3, y4]
 
-    m1, c1 = np.polyfit(np.log10(x), np.log10(y1), 1)
-    m2, c2 = np.polyfit(np.log10(x), np.log10(y2), 1)
-    m3, c3 = np.polyfit(np.log10(x), np.log10(y3), 1)
-    m4, c4 = np.polyfit(np.log10(x), np.log10(y4), 1)
-
-    ax[0,0].loglog(x, y1, linewidth=2, linestyle="--", marker="o", color="blue", label=f"grad. = {round(m1,4)}")
-    ax[0,1].loglog(x, y2, linewidth=2, linestyle="--", marker="o", color="red", label=f"grad. = {round(m2,4)}")
-    ax[1,0].loglog(x, y3, linewidth=2, linestyle="--", marker="o", color="green", label=f"grad. = {round(m3,4)}")
-    ax[1,1].loglog(x, y4, linewidth=2, linestyle="--", marker="o", color="darkviolet", label=f"grad. = {round(m4,4)}")
-
-    ax[0,0].legend(prop={'size': 14})
-    ax[0,1].legend(prop={'size': 14})
-    ax[1,0].legend(prop={'size': 14})
-    ax[1,1].legend(prop={'size': 14})
+    for index, (_i,_j) in enumerate(plotIndexes):
+        m, c = np.polyfit(np.log10(x), np.log10(y_data[index]), 1)
+        ax[_i,_j].loglog(x, y_data[index], linewidth=2, linestyle="--", marker="o", color=colours[index], label=f"grad. = {round(m,4)}")
+        ax[_i,_j].legend(prop={'size': 14})
 
     print(f"{generic.bcolours.OKGREEN}EOC (density){generic.bcolours.ENDC}: {np.diff(np.log(y1))/np.diff(np.log(x))}\n{generic.bcolours.OKGREEN}EOC (pressure){generic.bcolours.ENDC}: {np.diff(np.log(y2))/np.diff(np.log(x))}\n{generic.bcolours.OKGREEN}EOC (vx){generic.bcolours.ENDC}: {np.diff(np.log(y3))/np.diff(np.log(x))}\n{generic.bcolours.OKGREEN}EOC (thermal){generic.bcolours.ENDC}: {np.diff(np.log(y4))/np.diff(np.log(x))}")
 
@@ -258,18 +240,10 @@ def makeVideo(f, configVariables, testVariables, savepath, vidpath):
         for t, domain in simulation.items():
             fig, ax = plt.subplots(nrows=2, ncols=2, figsize=[21, 10])
 
-            ax[0,0].set_ylabel(r"Density $\rho$", fontsize=18)
-            ax[0,1].set_ylabel(r"Pressure $P$", fontsize=18)
-            ax[1,0].set_ylabel(r"Velocity $v_x$", fontsize=18)
-            ax[1,1].set_ylabel(r"Thermal energy $\frac{P}{\rho}$", fontsize=18)
-            ax[0,0].set_xlim([startPos, endPos])
-            ax[0,1].set_xlim([startPos, endPos])
-            ax[1,0].set_xlim([startPos, endPos])
-            ax[1,1].set_xlim([startPos, endPos])
-            ax[0,0].grid(linestyle='--', linewidth=0.5)
-            ax[0,1].grid(linestyle='--', linewidth=0.5)
-            ax[1,0].grid(linestyle='--', linewidth=0.5)
-            ax[1,1].grid(linestyle='--', linewidth=0.5)
+            for index, (_i,_j) in enumerate(plotIndexes):
+                ax[_i,_j].set_ylabel(labels[index], fontsize=18)
+                ax[_i,_j].set_xlim([startPos, endPos])
+                ax[_i,_j].grid(linestyle="--", linewidth=0.5)
 
             y1 = domain[:, 0]               # density
             y2 = domain[:, 4]               # pressure
@@ -277,10 +251,8 @@ def makeVideo(f, configVariables, testVariables, savepath, vidpath):
             y4 = domain[:, 4]/domain[:, 0]  # thermal energy
             x = np.linspace(startPos, endPos, N)
             y_data = [y1, y2, y3, y4]
-            colours = ["blue", "red", "green", "darkviolet"]
 
-            # density, pressure, vx, thermal energy
-            for index, (_i,_j) in enumerate([[0,0], [0,1], [1,0], [1,1]]):
+            for index, (_i,_j) in enumerate(plotIndexes):
                 if beautify:
                     gradient_plot([x,y_data[index]], [_i,_j], ax=ax, linewidth=2, color=colours[index])
                 else:
@@ -322,18 +294,10 @@ def makeVideo(f, configVariables, testVariables, savepath, vidpath):
 def plotInstance(domain, showPlot=True, text="", startPos=0, endPos=1, **kwargs):
     fig, ax = plt.subplots(nrows=2, ncols=2, figsize=[21, 10])
 
-    ax[0,0].set_ylabel(r"Density $\rho$", fontsize=18)
-    ax[0,1].set_ylabel(r"Pressure $P$", fontsize=18)
-    ax[1,0].set_ylabel(r"Velocity $v_x$", fontsize=18)
-    ax[1,1].set_ylabel(r"Thermal energy $\frac{P}{\rho}$", fontsize=18)
-    ax[0,0].set_xlim([startPos, endPos])
-    ax[0,1].set_xlim([startPos, endPos])
-    ax[1,0].set_xlim([startPos, endPos])
-    ax[1,1].set_xlim([startPos, endPos])
-    ax[0,0].grid(linestyle='--', linewidth=0.5)
-    ax[0,1].grid(linestyle='--', linewidth=0.5)
-    ax[1,0].grid(linestyle='--', linewidth=0.5)
-    ax[1,1].grid(linestyle='--', linewidth=0.5)
+    for index, (_i,_j) in enumerate(plotIndexes):
+        ax[_i,_j].set_ylabel(labels[index], fontsize=18)
+        ax[_i,_j].set_xlim([startPos, endPos])
+        ax[_i,_j].grid(linestyle="--", linewidth=0.5)
 
     y1 = domain[:, 0]   # density
     y2 = domain[:, 4]   # pressure
@@ -341,10 +305,8 @@ def plotInstance(domain, showPlot=True, text="", startPos=0, endPos=1, **kwargs)
     y4 = y2/y1          # thermal energy
     x = np.linspace(startPos, endPos, len(y1))
     y_data = [y1, y2, y3, y4]
-    colours = ["blue", "red", "green", "darkviolet"]
 
-    # density, pressure, vx, thermal energy
-    for index, (_i,_j) in enumerate([[0,0], [0,1], [1,0], [1,1]]):
+    for index, (_i,_j) in enumerate(plotIndexes):
         if beautify:
             gradient_plot([x,y_data[index]], [_i,_j], ax=ax, linewidth=2, color=colours[index])
         else:
@@ -395,4 +357,4 @@ def gradient_plot(data, plot_index, ax=None, fill_color=None, **kwargs):
     ax[i,j].add_patch(clip_path)
     im.set_clip_path(clip_path)
 
-    return line
+    pass
