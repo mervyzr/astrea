581002a8c675afe6e7603fdcb5ed6188ce6d3a58
Mervin Yap <mervin.yzr@hotmail.com>
2024-05-06 23:34:09 +0200

    chore: edited default value

--- simulate.py
+++ simulate.py
@@ -108,7 +108,7 @@ if __name__ == "__main__":
 
             if configVariables['savePlots']:
                 plotting.plotQuantities(f, configVariables, testVariables, savepath)
-                plotting.plotTotalVariation(f, configVariables, savepath)
+                #plotting.plotTotalVariation(f, configVariables, savepath)
                 if configVariables['runType'].startswith('m') and (configVariables['config'].startswith('sin') or configVariables['config'].startswith('gauss')):
                     plotting.plotSolutionErrors(f, configVariables, testVariables, savepath)
 
