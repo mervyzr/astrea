7529393d9c5abeeb8c41df4c7a17d536db695851
Mervin Yap <138670570+mervin-mobilab@users.noreply.github.com>
2024-08-08 14:25:11 +0200

    refactor: misc edits

--- numerics/solvers.py
+++ numerics/solvers.py
@@ -13,7 +13,7 @@ from functions import fv
 
 
 # Intercell numerical fluxes between L and R interfaces based on Riemann solver
-def calculateRiemannFlux(simVariables, *args, **kwargs):
+def calculateRiemannFlux(simVariables, **kwargs):
     Data = namedtuple('Data', ['flux', 'eigmax'])
 
     # Determine the eigenvalues for the computation of time stepping
@@ -85,7 +85,6 @@ def calculateHLLCFlux(wLs, wRs, simVariables):
     QL, QR = fv.convertPrimitive(wRs, simVariables), fv.convertPrimitive(wLs, simVariables)
     fL, fR = fv.makeFluxTerm(wRs, gamma), fv.makeFluxTerm(wLs, gamma)
 
-    zeta = (gamma-1)/(2*gamma)
     cL, cR = np.sqrt(gamma*fv.divide(pL, rhoL)), np.sqrt(gamma*fv.divide(pR, rhoR))
     u_hat = fv.divide(uL*np.sqrt(rhoL) + uR*np.sqrt(rhoR), np.sqrt(rhoL) + np.sqrt(rhoR))
     c2_hat = fv.divide(np.sqrt(rhoL)*cL**2 + np.sqrt(rhoR)*cR**2, np.sqrt(rhoL) + np.sqrt(rhoR)) + .5*((uR-uL)**2)*fv.divide(np.sqrt(rhoL)*np.sqrt(rhoR), (np.sqrt(rhoL)+np.sqrt(rhoR))**2)
