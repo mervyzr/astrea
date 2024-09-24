6d999e764ef174b3ff6164833d343a73e55f8708
mervyzr <mervin.yzr@hotmail.com>
2024-09-24 16:16:05 +0200

    fix: WENO runs without crashing

--- schemes/weno.py
+++ schemes/weno.py
@@ -12,38 +12,39 @@ def run(grid, sim_variables):
     nested_dict = lambda: defaultdict(nested_dict)
     data = nested_dict()
 
-    # Function to generate the WENO interface values
-    def extrapolate_face_value(_wS, _boundary):
-        # Pad array with boundary
-        w2 = fv.add_boundary(_wS, _boundary, 2)
+    # Function to generate the WENO interface values for u_i+1/2
+    def reconstruct(_wS, _boundary):
+        eps = 1e-6
+        w = fv.add_boundary(_wS, _boundary, 2)
 
         # Define frequently used terms
-        minus_one, minus_two = w2[1:-3], w2[:-4]
-        plus_one, plus_two = w2[3:-1], w2[4:]
+        minus_one, minus_two = w[1:-3], w[:-4]
+        plus_one, plus_two = w[3:-1], w[4:]
+        zeroth = w[2:-2]
 
         # Define the stencils
-        u1 = (minus_two/3) - (minus_one*7/6) + (_wS*11/6)
-        u2 = -(minus_one/6) + (_wS*5/6) + (plus_one/3)
-        u3 = (_wS/3) + (plus_one*5/6) - (plus_two/6)
+        u1 = 1/6 * (2*minus_two - 7*minus_one + 11*zeroth)
+        u2 = 1/6 * (-minus_one + 5*zeroth + 2*plus_one)
+        u3 = 1/6 * (2*zeroth + 5*plus_one - plus_two)
 
         # Define the linear weights
-        x1, x2, x3 = 1/10, 3/5, 3/10
+        g1, g2, g3 = .1, .6, .3
 
         # Determine the smoothness indicators
-        b1 = (13/12 * (minus_two - 2*minus_one + _wS)**2) + (.25 * (minus_two - 4*minus_one + 3*_wS)**2)
-        b2 = (13/12 * (minus_one - 2*_wS + plus_one)**2) + (.25 * (minus_one - plus_one)**2)
-        b3 = (13/12 * (_wS - 2*plus_one + plus_two)**2) + (.25 * (3*_wS - 4*plus_one + plus_two)**2)
+        b1 = 1/12 * (13*(minus_two - 2*minus_one + zeroth)**2 + 3*(minus_two - 4*minus_one + 3*zeroth)**2)
+        b2 = 1/12 * (13*(minus_one - 2*zeroth + plus_one)**2 + 3*(minus_one - plus_one)**2)
+        b3 = 1/12 * (13*(zeroth - 2*plus_one + plus_two)**2 + 3*(3*zeroth - 4*plus_one + plus_two)**2)
 
-        # Determine the non-linear weights
-        alpha1 = x1/((1e-6 + b1)**2)
-        alpha2 = x2/((1e-6 + b2)**2)
-        alpha3 = x3/((1e-6 + b3)**2)
+        # Define the non-linear weights
+        a1 = g1/((eps + b1)**2)
+        a2 = g2/((eps + b2)**2)
+        a3 = g3/((eps + b3)**2)
 
-        weight1 = fv.divide(alpha1, alpha1+alpha2+alpha3)
-        weight2 = fv.divide(alpha2, alpha1+alpha2+alpha3)
-        weight3 = fv.divide(alpha3, alpha1+alpha2+alpha3)
+        w1 = a1/(a1+a2+a3)
+        w2 = a2/(a1+a2+a3)
+        w3 = a3/(a1+a2+a3)
 
-        return weight1*u1 + weight2*u2 + weight3*u3
+        return w1*u1 + w2*u2 + w3*u3
 
     # Rotate grid and apply algorithm for each axis
     for axis, axes in enumerate(permutations):
@@ -52,18 +53,19 @@ def run(grid, sim_variables):
         # Convert to primitive variables
         wS = fv.convert_conservative(_grid, sim_variables)
 
-        # Pad array with boundary
-        w = fv.add_boundary(wS, boundary)
-
         """WENO reconstruction [Shu, 2009]
         Current convention: |               w(i-1/2)                    w(i+1/2)              |
                             | i-1          <-- | -->         i         <-- | -->          i+1 |
                             |        w_R(i-1)  |   w_L(i)          w_R(i)  |  w_L(i+1)        |
+                    OR      |       w-(i-1/2)  |   w+(i-1/2)    w-(i+1/2)  |  w+(i+1/2)       |
         """
-        wL, wR = extrapolate_face_value(w[2:], boundary), extrapolate_face_value(w[1:-1], boundary)
+        weno_approx = reconstruct(wS, boundary)
 
-        # Pad the reconstructed interfaces
-        wLs, wRs = fv.add_boundary(wL, boundary)[1:], fv.add_boundary(wR, boundary)[:-1]
+        # Pad arrays with boundary
+        w = fv.add_boundary(wS, boundary)
+        _w = fv.add_boundary(weno_approx, boundary)
+
+        wLs, wRs = _w[1:], _w[:-1]
 
         # Convert the primitive variables
         qLs, qRs = fv.convert_primitive(wLs, sim_variables), fv.convert_primitive(wRs, sim_variables)
