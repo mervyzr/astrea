e15433ebe3591db2113d685bb91833f2ce516313
Mervin Yap <mervin.yzr@hotmail.com>
2024-10-31 00:20:19 +0100

    test: swapped configuration for Kelvin-Helmholtz set-up

--- static/tests.py
+++ static/tests.py
@@ -101,13 +101,13 @@ def generate_test_conditions(config, cells):
         misc = None
 
     elif "kelvin" in config or "helmholtz" in config or config == "khi":
-        start_pos = -1
+        start_pos = 0
         end_pos = 1
         shock_pos = .5
         t_end = 1
         boundary = "wrap"  # periodic
-        initial_left = np.array([2,-.5,0,0,2.5,0,0,0])
-        initial_right = np.array([1,.5,0,0,2.5,0,0,0])
+        initial_left = np.array([1,.5,0,0,2.5,0,0,0])
+        initial_right = np.array([2,-.5,0,0,2.5,0,0,0])
         misc = {'perturb_ampl':.01, 'freq':2}
 
     # [Yee et. al., 1999]
