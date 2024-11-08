06d521666be28dc5b9f0ce2809de34a776199870
Mervin Yap <138670570+mervin-mobilab@users.noreply.github.com>
2024-11-08 15:45:22 +0100

    Revert "build: moved to pyproject.toml implementation instead of setup.py"
    This reverts commit 0052c7223235cd29aa384542d3c202ab0d9faffa.

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
+#!/usr/bin/env python3
+
 import os
 import shutil
 import subprocess
-import setuptools
+from setuptools import setup, find_packages
 from setuptools.command.install import install
 
 
 CURRENTDIR = os.getcwd()
+
+with open(f"{CURRENTDIR}/README.md", "r") as f:
+    long_description = f.read()
+
 
 class CustomInstallCommand(install):
     """Customised setuptools install command"""
@@ -15,4 +21,33 @@ class CustomInstallCommand(install):
         subprocess.run(f"chmod +x {CURRENTDIR}/simulate.py", shell=True)
         install.run(self)
 
-setuptools.setup(cmdclass={"install": CustomInstallCommand,})
+
+setup(
+    name="mHydyS",
+    version="1.1.1",
+    author="Mervin Yap",
+    author_email="myap@ph1.uni-koeln.de",
+    description="(Magneto-)hydrodynamics code for simulating shocks in the ISM",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="<https://github.com/mervyzr/mHydyS>",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "scipy",
+        "h5py",
+        "matplotlib",
+        "pyyaml",
+        "tinydb",
+        "python-dotenv",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: GPLv3",
+        "Operating System :: Linux :: macOS",
+    ],
+    python_requires='>=3.10',
+    cmdclass={
+        "install": CustomInstallCommand,
+    },
+)
