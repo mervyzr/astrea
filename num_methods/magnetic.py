49e58e2432a81dd846f7296281dfeafff11e353e
mervyzr <mervin.yzr@hotmail.com>
2024-12-09 18:19:46 +0100

    refactor: added magnetic field computation

---
+++ num_methods/magnetic.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+from functions import fv
+
+##############################################################################
+# Fourth-order upwind constrained transport algorithm for MHD [Felker & Stone, 2018]
+##############################################################################
+
+# Compute the corner electric fields; gives 4-fold values for each corner
+def corner_E():
+    pass
