77a2057539f491100ace47619afe17f91e56aecc
mervyzr <mervin.yzr@hotmail.com>
2024-12-10 19:19:41 +0100

    feat: 2D MHD (incomplete)

--- num_methods/magnetic.py
+++ num_methods/magnetic.py
@@ -7,5 +7,9 @@ from functions import fv
 ##############################################################################
 
 # Compute the corner electric fields; gives 4-fold values for each corner
-def corner_E():
-    pass
+def corner_E(grid, sim_variables, method="ppm"):
+    gamma, permutations = sim_variables.gamma, sim_variables.permutations
+
+    for axis, axes in enumerate(permutations[1:] + permutations[:1]):
+        _grid = grid.transpose(axes)
+        pass
