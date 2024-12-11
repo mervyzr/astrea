891e462ef95691a3cbcfc3f6617c0d3d53c7e6a6
Mervin Yap <138670570+mervin-mobilab@users.noreply.github.com>
2024-12-11 17:07:19 +0100

    feat: 2D MHD (incomplete)

--- num_methods/magnetic.py
+++ num_methods/magnetic.py
@@ -1,25 +1,22 @@
 import numpy as np
 
 from functions import fv
+from num_methods import limiters
 
 ##############################################################################
 # Fourth-order upwind constrained transport algorithm for MHD [Felker & Stone, 2018]
 ##############################################################################
 
 # Compute the corner electric fields; gives 4-fold values for each corner
-def corner_E(grid, sim_variables, method="ppm"):
-    gamma, permutations = sim_variables.gamma, sim_variables.permutations
+def corner_E(wFs, axes, sim_variables, method="ppm"):
+    wF = np.copy(wFs.transpose(axes))
+    wF2 = fv.add_boundary(wF, sim_variables.boundary, 2)
+    wF1 = np.copy(wF2[1:-1])
 
-    for axis, axes in enumerate(permutations[1:] + permutations[:1]):
-        _grid = grid.transpose(axes)
-        pass
+    wF_D = 7/12 * (wF1[:-2] + wF) - 1/12 * (wF2[:-4] + wF1[2:])
+    wF_U = 7/12 * (wF + wF1[2:]) - 1/12 * (wF1[:-2] + wF2[4:])
 
+    limited_wFs_UD = limiters.interface_limiter(wF_D, wF2[:-4], wF1[:-2], wF, wF1[2:]), limiters.interface_limiter(wF_U, wF1[:-2], wF, wF1[2:], wF2[4:])
 
-        if sim_variables.magnetic and sim_variables.dimension > 1:
-            next_axes = permutations[(axis+1) % len(permutations)]
-            wF_transverse = np.copy(wF.transpose(next_axes))
-            wF_transverse_2 = fv.add_boundary(wF_transverse, boundary, 2)
-            wF_transverse_1 = np.copy(wF_transverse_2[1:-1])
-
-            wF_U = 7/12 * (wF_transverse + wF_transverse_1[2:]) - 1/12 * (wF_transverse_1[:-2] + wF_transverse_2[4:])
-            pass
+    kwargs = {"wF_pad2":fv.add_boundary(wF, sim_variables.boundary, 2) , "boundary":sim_variables.boundary}
+    wD, wU = limiters.interpolant_limiter(wF, wF1, wF2, "mc", *limited_wFs_UD, **kwargs)
