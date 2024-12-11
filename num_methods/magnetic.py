046f430ab8416f84a3de01be6e283db7c7c5bb42
Mervin Yap <138670570+mervin-mobilab@users.noreply.github.com>
2024-12-11 16:20:47 +0100

    feat: 2D MHD

--- num_methods/magnetic.py
+++ num_methods/magnetic.py
@@ -13,3 +13,13 @@ def corner_E(grid, sim_variables, method="ppm"):
     for axis, axes in enumerate(permutations[1:] + permutations[:1]):
         _grid = grid.transpose(axes)
         pass
+
+
+        if sim_variables.magnetic and sim_variables.dimension > 1:
+            next_axes = permutations[(axis+1) % len(permutations)]
+            wF_transverse = np.copy(wF.transpose(next_axes))
+            wF_transverse_2 = fv.add_boundary(wF_transverse, boundary, 2)
+            wF_transverse_1 = np.copy(wF_transverse_2[1:-1])
+
+            wF_U = 7/12 * (wF_transverse + wF_transverse_1[2:]) - 1/12 * (wF_transverse_1[:-2] + wF_transverse_2[4:])
+            pass
