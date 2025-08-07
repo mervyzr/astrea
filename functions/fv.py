c22a19e0ac2cfb1b42578e01c57685b1d8ab0262
Mervin Yap <mervin.yzr@hotmail.com>
2025-08-07 16:20:11 +0200

    refactor: renamed variables

--- functions/fv.py
+++ functions/fv.py
@@ -227,14 +227,14 @@ def compute_eigmax(characteristics, axis):
 
 
 # Calculate the Roe-averaged primitive variables at the interface from the minus- & plus-interface states for use in Roe solver in order to better capture shocks [Roe & Pike, 1984; Brio & Wu, 1988; LeVeque, 2002; Stone et al., 2008]
-def compute_Roe_average(left_interface, right_interface):
+def compute_Roe_average(plus_interface, minus_interface):
     rho, pressure, vels, Bfields = 0, 4, slice(1,4), slice(5,8)
-    avg = np.zeros_like(left_interface)
-    rho_minus, rho_plus = np.sqrt(right_interface[...,rho]), np.sqrt(left_interface[...,rho])
+    avg = np.zeros_like(plus_interface)
+    rho_plus, rho_minus = np.sqrt(plus_interface[...,rho]), np.sqrt(minus_interface[...,rho])
 
     avg[...,rho] = rho_minus * rho_plus
-    avg[...,vels] = divide((left_interface[...,vels] * rho_plus[...,None]) + (right_interface[...,vels] * rho_minus[...,None]), (rho_minus + rho_plus)[...,None])
-    avg[...,pressure] = divide((rho_plus * left_interface[...,pressure]) + (rho_minus * right_interface[...,pressure]), rho_minus + rho_plus)
-    avg[...,Bfields] = divide((left_interface[...,Bfields] * rho_minus[...,None]) + (right_interface[...,Bfields] * rho_plus[...,None]), (rho_minus + rho_plus)[...,None])
+    avg[...,vels] = divide((plus_interface[...,vels] * rho_plus[...,None]) + (minus_interface[...,vels] * rho_minus[...,None]), (rho_minus + rho_plus)[...,None])
+    avg[...,pressure] = divide((rho_plus * plus_interface[...,pressure]) + (rho_minus * minus_interface[...,pressure]), rho_minus + rho_plus)
+    avg[...,Bfields] = divide((plus_interface[...,Bfields] * rho_minus[...,None]) + (minus_interface[...,Bfields] * rho_plus[...,None]), (rho_minus + rho_plus)[...,None])
 
     return avg
