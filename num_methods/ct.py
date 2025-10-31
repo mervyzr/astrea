fix: corrected CT computation; MHD runs more stable

num_methods/ct.py
@@ -4,7 +4,6 @@ from itertools import repeat
 import numpy as np
 
 from functions import fv
-from num_methods import limiters
 from schemes import pcm, plm, ppm, weno, cweno
 
 ##############################################################################
@@ -19,123 +18,101 @@ def compute_alphas(characteristics, axis):
     return fv.slice_(np.maximum(0, max_eigvals), axis, start=1), fv.slice_(-np.minimum(0, min_eigvals), axis, start=1)
 
 
-# Wrapper for reconstruct_transverse; for each orthogonal axis, it will compute for each pair of (+) & (-) interfaces
-def reconstruct_wrapper(interfaces, sim_variables, ortho_axes):
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        jobs = executor.map(reconstruct_transverse, repeat(interfaces), repeat(sim_variables), ortho_axes)
-        return {ortho_axes[idx]:ortho_interfaces for idx, ortho_interfaces in enumerate(jobs)}
-
 # Reconstruct the transverse values for each face average (computation done entirely for orthogonal axis)
 # Note that this reconstruction is done at the INTERFACES, NOT CENTRES, and it is for one orthogonal axis
-def reconstruct_transverse(interfaces, sim_variables, ortho_axis, method=None, eta=None):
-    if not method:
-        method = sim_variables.subgrid_category
+def reconstruct_transverse(data, sim_variables, axis, method=None, eta=None):
+    _axes = np.array(range(3))
+    ortho_axes = _axes[_axes != axis]
+    normal_axes = np.roll(ortho_axes, shift=-1)
 
-    """Interpolate the face averages to both corners (upwards & downwards)
-    |                w(i-1/2)            w(i+1/2)               |
-    |-------------------|-------------------|-------------------|
-    |           w_U(i-1/2,j+1/2)    w_U(i+1/2,j+1/2)            |
-    |                  ^|                  ^|                  ^|
-    |                  ||                  ||                  ||
-    |                  ||                  ||                  ||
-    |  o (i-1,j)     -->|  o (i,j)       -->|  o (i+1,j)     -->|
-    |                  ||                  ||                  ||
-    |                  ||                  ||                  ||
-    |                  v|                  v|                  v|
-    |           w_D(i-1/2,j-1/2)    w_D(i+1/2,j-1/2)            |
-    |-------------------|-------------------|-------------------|
+    ortho_interfaces = {}
+    if not None in list(map(data.get, ortho_axes)):
+        if not method:
+            method = sim_variables.subgrid_category
 
-        OR
+        """Interpolate the face averages to both corners (upwards & downwards)
+        |                w(i-1/2)            w(i+1/2)               |
+        |-------------------|-------------------|-------------------|
+        |           w_U(i-1/2,j+1/2)    w_U(i+1/2,j+1/2)            |
+        |                  ^|                  ^|                  ^|
+        |                  ||                  ||                  ||
+        |                  ||                  ||                  ||
+        |  o (i-1,j)     -->|  o (i,j)       -->|  o (i+1,j)     -->|
+        |                  ||                  ||                  ||
+        |                  ||                  ||                  ||
+        |                  v|                  v|                  v|
+        |           w_D(i-1/2,j-1/2)    w_D(i+1/2,j-1/2)            |
+        |-------------------|-------------------|-------------------|
 
-    Reconstruct the limited extrapolants from the interface values. Returns the face averages in the form of w+(y) & w-(y) when considering x-axis, and w+(x) & w-(x) when considering y-axis
-    |                w(i-1/2)            w(i+1/2)               |
-    |  o (i-1,j+1)      |  o (i,j+1)        |  o (i+1,j+1)      |
-    |                   |                   |                   |
-    |                   |                   |                   |
-    |           w_D(i-1/2,j+1/2)    w_D(i+1/2,j+1/2)            |
-    |                 w+(y)               w+(y)               w+(y)
-    |                   ^                   ^                   ^
-    |-------------------|-------------------|-------------------|
-    |                   v                   v                   v
-    |                 w-(y)               w-(y)               w-(y)
-    |           w_U(i-1/2,j+1/2)    w_U(i+1/2,j+1/2)            |
-    |                   |                   |                   |
-    |                   |                   |                   |
-    |  o (i-1,j)     -->|  o (i,j)       -->|  o (i+1,j)     -->|
-    """
-    if method == "weno":
-        reconstruct = weno.reconstruct
-    elif method == "cweno":
-        reconstruct = cweno.reconstruct
-    elif method == "ppm":
-        if sim_variables.ppm_dissipate:
-            reconstruct = lambda _grid, _sim_variables, _axis: ppm.reconstruct(_grid, _sim_variables, _axis, eta=eta)
+            OR
+
+        Reconstruct the limited extrapolants from the interface values. Returns the face averages in the form of w+(y) & w-(y) when considering x-axis, and w+(x) & w-(x) when considering y-axis
+        |                w(i-1/2)            w(i+1/2)               |
+        |  o (i-1,j+1)      |  o (i,j+1)        |  o (i+1,j+1)      |
+        |                   |                   |                   |
+        |                   |                   |                   |
+        |           w_D(i-1/2,j+1/2)    w_D(i+1/2,j+1/2)            |
+        |                 w+(y)               w+(y)               w+(y)
+        |                   ^                   ^                   ^
+        |-------------------|-------------------|-------------------|
+        |                   v                   v                   v
+        |                 w-(y)               w-(y)               w-(y)
+        |           w_U(i-1/2,j+1/2)    w_U(i+1/2,j+1/2)            |
+        |                   |                   |                   |
+        |                   |                   |                   |
+        |  o (i-1,j)     -->|  o (i,j)       -->|  o (i+1,j)     -->|
+        """
+        if method == "weno":
+            reconstruct = lambda _grid, _sim_variables, _axis: weno.reconstruct(_grid, _sim_variables, _axis, order=5)
+        elif method == "cweno":
+            reconstruct = cweno.reconstruct
+        elif method == "ppm":
+            if sim_variables.ppm_dissipate:
+                reconstruct = lambda _grid, _sim_variables, _axis: ppm.reconstruct(_grid, _sim_variables, _axis, eta=eta)
+            else:
+                reconstruct = ppm.reconstruct
+        elif method == "plm":
+            reconstruct = plm.reconstruct
         else:
-            reconstruct = ppm.reconstruct
-    elif method == "plm":
-        reconstruct = plm.reconstruct
-    else:
-        reconstruct = pcm.reconstruct
+            reconstruct = pcm.reconstruct
 
-    transverse_interfaces = []  # should have 4 interfaces per ortho_axis; 2 transverse interfaces for each longitudinal reconstruction
-    with concurrent.futures.ThreadPoolExecutor() as inner_executor:
-        jobs = inner_executor.map(reconstruct, interfaces, repeat(sim_variables), repeat(ortho_axis))
+        interfaces = [data[axis]['avgd_interfaces'] for axis in ortho_axes]
+        with concurrent.futures.ThreadPoolExecutor() as inner_executor:
+            jobs = inner_executor.map(reconstruct, interfaces, repeat(sim_variables), normal_axes)
+            for idx, [wD, wU] in enumerate(jobs):
+                normal_axis = normal_axes[idx]
 
-        for [wD, wU] in jobs:
-            # Re-align the interfaces so that cell wall is in between interfaces
-            prim_plus, prim_minus = fv.slice_(fv.add_boundary(wD, sim_variables, axis=ortho_axis), ortho_axis, start=1), fv.slice_(fv.add_boundary(wU, sim_variables, axis=ortho_axis), ortho_axis, end=-1)
+                # Re-align the interfaces so that cell wall is in between interfaces
+                prim_plus, prim_minus = fv.slice_(fv.add_boundary(wD, sim_variables, axis=normal_axis), normal_axis, start=1), fv.slice_(fv.add_boundary(wU, sim_variables, axis=normal_axis), normal_axis, end=-1)
 
-            # Remove the 'leftmost' interface since only the upwind corners/lines are needed
-            prim_plus, prim_minus = fv.slice_(prim_plus, axis=ortho_axis, start=1), fv.slice_(prim_minus, axis=ortho_axis, start=1)
+                # Remove the 'leftmost' interface since only the upwind corners/lines are needed
+                prim_plus, prim_minus = fv.slice_(prim_plus, axis=normal_axis, start=1), fv.slice_(prim_minus, axis=normal_axis, start=1)
 
-            transverse_interfaces.append([prim_plus, prim_minus])
+                ortho_interfaces[normal_axis] = prim_plus, prim_minus
 
-    return transverse_interfaces
+    return ortho_interfaces
 
 
-
-
-
-# CONDITIONALS ARE HANDLING EVERYTHING! SO DON'T NEEDA WORRY ABOUT DIMENSION = 1
-# Dimension = 1, axes = [0]
-# axis = 0 => ortho_axes = [] => results = {}
-# axis = 1 => won't compute
-# axis = 2 => won't compute
-# So this seems possible; the reconstruct_transverse will not reconstruct anything
-#
-# Dimension = 2, axes = [0,1]
-# axis = 0 => ortho_axes = [1] => results = {1:[[w_y++,w_y+-] , [w_y-+,w_y--]]}
-# axis = 1 => ortho_axes = [0] => results = {0:[[w_x++,w_x+-] , [w_x-+,w_x--]]}
-# axis = 2 => won't compute
-# Seems possible too; the emf will try and compute for all axes, which will only succeed for z-axis bc only x- & y-axis info are available
-#
-# Dimension = 3, axes = [0,1,2]
-# axis = 0 => ortho_axes = [1,2] => results = {1:[[w_y++,w_y+-] , [w_y-+,w_y--]] , 2:[[w_z++,w_z+-] , [w_z-+,w_z--]]}
-# axis = 1 => ortho_axes = [0,2] => results = {0:[[w_x++,w_x+-] , [w_x-+,w_x--]] , 2:[[w_z++,w_z+-] , [w_z-+,w_z--]]}
-# axis = 2 => ortho_axes = [0,1] => results = {0:[[w_x++,w_x+-] , [w_x-+,w_x--]] , 1:[[w_y++,w_y+-] , [w_y-+,w_y--]]}
-# Seems possible; the emf will still try and compute for all axes, but now all axes will succeed. I think?
-
-
-
-
-# Compute the corner/line electric fields wrt to corner/line for each axis [Mignone & Del Zanna, 2020]
-def compute_emf(data, axis):
+# Compute the corner/line electric fields wrt to corner/line for each axis [Verma et al., 2018; Mignone & Del Zanna, 2020]
+def compute_emf(ortho_interfaces, alphas, axis):
     abscissa, ordinate, applicate = (axis + np.array(range(3)))%3
 
-    # Assume computation for the z-axis EMF here (axis=2), and so x-axis (ordinate) & y-axis (applicate) are needed
-    vx, vy = 1+ordinate, 1+applicate
-    Bx, By = 5+ordinate, 5+applicate
+    axis_data = ortho_interfaces[abscissa]
 
-    # For -v x B in x-axis (axis=0, thumb), y (axis=1, middle finger) becomes (N,S) & z (axis=2, index finger) becomes (E,W)
-    # For -v x B in y-axis (axis=1, thumb), z (axis=2, middle finger) becomes (N,S) & x (axis=0, index finger) becomes (E,W)
-    # For -v x B in z-axis (axis=2, thumb), x (axis=0, middle finger) becomes (N,S) & y (axis=1, index finger) becomes (E,W)
+    # For -v x B in x-axis (axis=0, thumb), Bz along y (axis=1, index finger) becomes (E,W) & By along z (axis=2, middle finger) becomes (N,S)
+    # For -v x B in y-axis (axis=1, thumb), Bx along z (axis=2, index finger) becomes (E,W) & Bz along x (axis=0, middle finger) becomes (N,S)
+    # For -v x B in z-axis (axis=2, thumb), By along x (axis=0, index finger) becomes (E,W) & Bx along y (axis=1, middle finger) becomes (N,S)
     try:
-        [north, south], [east, west] = data[applicate]['ortho_interfaces'], data[ordinate]['ortho_interfaces']
-        [ap_x, am_x], [ap_y, am_y] = data[ordinate]['alphas'], data[applicate]['alphas']
-    except KeyError:
+        # Assume computation for the z-axis EMF here (axis=2), and so x-axis (ordinate) & y-axis (applicate) are needed
+        [north, south], [east, west] = axis_data[applicate], axis_data[ordinate]
+        [ap_x, am_x], [ap_y, am_y] = alphas[ordinate], alphas[applicate]
+    except (KeyError, TypeError):
         # For 2D cases where the z-axis is missing; make a zeros_like data from the x- or y-axis instead
-        emf = np.zeros_like(data[abscissa]['ortho_interfaces'][0][...,vx])
+        emf = np.zeros_like(alphas[0][0])
     else:
+        vx, vy = 1+ordinate, 1+applicate
+        Bx, By = 5+ordinate, 5+applicate
+
         # Compute the corner mag. fields wrt to corner/line
         SW = .5*(west[...,vy]+south[...,vy])*south[...,Bx] - .5*(west[...,vx]+south[...,vx])*west[...,By]
         SE = .5*(east[...,vy]+south[...,vy])*south[...,Bx] - .5*(east[...,vx]+south[...,vx])*east[...,By]

num_methods/evolvers.py
@@ -62,16 +62,22 @@ def evolve_space(grid, sim_variables, first_stage=False):
 
     # Magnetohydrodynamics computation
     if magnetic and multidimensional:
-        # The proper assignment of the corners is important for directional updates, so the dict keys are used for this assignment
         with concurrent.futures.ThreadPoolExecutor() as executor:
-            emf_jobs = executor.map(ct.compute_emf, repeat(data), range(3))
+            alphas = {axis:data[axis]['alphas'] for axis in axes}
+
+            # Magnetic transverse interfaces reconstructed along orthogonal axis/axes; use the averaged (+) & (-) values
+            ortho_jobs = executor.map(ct.reconstruct_transverse, repeat(data), repeat(sim_variables), range(3))
+            ortho_interfaces = {axis:ortho_interfaces for axis, ortho_interfaces in enumerate(ortho_jobs)}
+
+            # The proper assignment of the corners is important for directional updates, so the dict keys are used for this assignment
+            emf_jobs = executor.map(ct.compute_emf, repeat(ortho_interfaces), repeat(alphas), range(3))
             emfs = np.asarray([emf for emf in emf_jobs])
 
             # Update fluxes with CT implementation; lists ordered according to axes
             emf_flux_jobs = executor.map(ct.compute_ct_flux, fluxes, repeat(emfs), repeat(sim_variables), range(3))
             fluxes = [emf_flux for emf_flux in emf_flux_jobs]
 
-    # Calculate the total fluxes through all upwind surfaces [F(i+1/2,j) - F(i-1/2,j)]/dx, [G(i,j+1/2) - G(i,j-1/2)]/dy
+    # Calculate the total fluxes through all upwind surfaces [F(i+1/2,j,k) - F(i-1/2,j,k)]/dx, [G(i,j+1/2,k) - G(i,j-1/2,k)]/dy, [H(i,j,k+1/2) - H(i,j,k-1/2)]/dz
     total_flux = -np.sum(fluxes, axis=0)
 
     if first_stage:

schemes/cweno.py
@@ -79,10 +79,6 @@ def run(grid, sim_variables, axis):
         padded_grid = fv.add_boundary(grid, sim_variables, axis=axis)
         prim_plus[...,5+axes] = prim_minus[...,5+axes] = fv.slice_(padded_grid, axis, end=-1)[...,5+axes]
 
-        # Magnetic transverse interfaces reconstructed along orthogonal axis/axes
-        if multidimensional:
-            data['ortho_interfaces'] = ct.reconstruct_wrapper([prim_plus, prim_minus], sim_variables, ortho_axes)
-
     # Get the average solution between the interfaces at the boundaries
     intf_avg = fv.compute_Roe_average([prim_plus,prim_minus], sim_variables)
     padded_intf_avg = fv.add_boundary(fv.slice_(intf_avg, axis, start=1), sim_variables, axis=axis)
@@ -97,8 +93,11 @@ def run(grid, sim_variables, axis):
     # Compute eigmax for time stepping limits
     characteristics = np.linalg.eigvals(jacobian)
     data['eigmax'] = ds[axis]/fv.compute_eigmax(characteristics, axis=axis)
-    if magnetic:
+
+    # Compute alphas and save reconstructed (averaged) interfaces for CT computation
+    if magnetic and multidimensional:
         data['alphas'] = ct.compute_alphas(characteristics, axis=axis)
+        data['avgd_interfaces'] = fv.slice_(intf_avg, axis, start=1)
 
     # Calculate the interface-averaged fluxes
     intf_fluxes_avgd = Riemann_solver(axis, sim_variables, **{

schemes/pcm.py
@@ -17,7 +17,6 @@ def run(grid, sim_variables, axis):
     data = {}
 
     Riemann_solver = solvers.get_Riemann_solver(sim_variables)
-    ortho_axes = axes[axes != axis] if (magnetic or multidimensional) else None
 
     # Pad array with boundaries
     padded_primitive = fv.add_boundary(grid, sim_variables, axis=axis)
@@ -31,16 +30,14 @@ def run(grid, sim_variables, axis):
     characteristics = np.linalg.eigvals(jacobian)
     data['eigmax'] = ds[axis]/fv.compute_eigmax(characteristics, axis=axis)
 
-    prim_interfaces = [fv.slice_(padded_primitive, axis, start=1), fv.slice_(padded_primitive, axis, end=-1)]
-
-    # Magnetic transverse interfaces reconstructed along orthogonal axis/axes (interface = centre for PCM)
+    # Compute alphas and save reconstructed (averaged) interfaces for CT computation (interface = centre for PCM)
     if magnetic and multidimensional:
-        data['ortho_interfaces'] = ct.reconstruct_wrapper(prim_interfaces, sim_variables, ortho_axes)
         data['alphas'] = ct.compute_alphas(characteristics, axis=axis)
+        data['avgd_interfaces'] = np.copy(grid)
 
     # Calculate the interface-averaged fluxes (pointwise & averaged values are the same for lower-order schemes)
     intf_fluxes_avgd = intf_fluxes_cntrd = Riemann_solver(axis, sim_variables, **{
-        'prim_interfaces': prim_interfaces,
+        'prim_interfaces': [fv.slice_(padded_primitive, axis, start=1), fv.slice_(padded_primitive, axis, end=-1)],
         'cons_interfaces': [fv.slice_(padded_conservative, axis, start=1), fv.slice_(padded_conservative, axis, end=-1)],
         'flux_interfaces': [fv.slice_(fluxes, axis, start=1), fv.slice_(fluxes, axis, end=-1)],
         'characteristics': characteristics,

schemes/plm.py
@@ -29,7 +29,6 @@ def run(grid, sim_variables, axis):
     data = {}
 
     Riemann_solver = solvers.get_Riemann_solver(sim_variables)
-    ortho_axes = axes[axes != axis] if (magnetic or multidimensional) else None
 
     # Linear reconstruction [Derigs et al., 2017]
     wL, wR = reconstruct(grid, sim_variables, axis=axis)
@@ -40,12 +39,8 @@ def run(grid, sim_variables, axis):
         padded_grid = fv.add_boundary(grid, sim_variables, axis=axis)
         prim_plus[...,5+axes] = prim_minus[...,5+axes] = fv.slice_(padded_grid, axis, end=-1)[...,5+axes]
 
-        # Magnetic transverse interfaces reconstructed along orthogonal axis/axes
-        if multidimensional:
-            data['ortho_interfaces'] = ct.reconstruct_wrapper([prim_plus, prim_minus], sim_variables, ortho_axes)
-
     # Get the average solution between the interfaces at the boundaries
-    intf_avg = .5* (prim_plus + prim_minus)
+    intf_avg = .5 * (prim_plus + prim_minus)
     padded_intf_avg = fv.add_boundary(fv.slice_(intf_avg, axis, start=1), sim_variables, axis=axis)
 
     # Convert the primitive interface variables
@@ -58,8 +53,11 @@ def run(grid, sim_variables, axis):
     # Compute eigmax for time stepping limits
     characteristics = np.linalg.eigvals(jacobian)
     data['eigmax'] = ds[axis]/fv.compute_eigmax(characteristics, axis=axis)
-    if magnetic:
+
+    # Compute alphas and save reconstructed (averaged) interfaces for CT computation
+    if magnetic and multidimensional:
         data['alphas'] = ct.compute_alphas(characteristics, axis=axis)
+        data['avgd_interfaces'] = fv.slice_(intf_avg, axis, start=1)
 
     # Calculate the interface-averaged fluxes (pointwise & averaged values are the same for lower-order schemes)
     intf_fluxes_avgd = intf_fluxes_cntrd = Riemann_solver(axis, sim_variables, **{