a9beff23fe4d0c18dc07f097705aaadaa9b7acac
Mervin Yap <mervin.yzr@hotmail.com>
2026-05-09 17:43:01 +0200

    refactor: re-arranged tests.py

--- static/tests.py
+++ static/tests.py
@@ -9,37 +9,10 @@ def generate_test_conditions(config_variables):
     config, cells, gamma = config_variables['config'], config_variables['cells'], config_variables['gamma']
     match = lambda match_type, substrings: match_type(substring in config for substring in substrings)
 
-    # [Sod, 1978]
-    if "sod" in config:
-        axis_coord = [0,1]
-        shock_pos = .5
-        t_end = .2
-        boundary = "edge"
-        init_cond = np.array([1,0,0,0,1,0,0,0])
-        ambient = np.array([.125,0,0,0,.1,0,0,0])
-        misc = None
-
-    # [Sedov, 1959]
-    elif "sedov" in config:
-        axis_coord = [-10,10]
-        shock_pos = .5
-        t_end = 2
-        boundary = "wrap"
-        init_cond = np.array([1,0,0,0,100,0,0,0])
-        ambient = np.array([1,0,0,0,1e-12,0,0,0])
-        misc = None
-
-    # [Shu & Osher, 1989]
-    elif match(any, ["shu", "osher"]) or config == "so":
-        axis_coord = [-1,1]
-        shock_pos = -.8
-        t_end = .47
-        boundary = "edge"
-        init_cond = np.array([3.857143,2.629369,0,0,10.3333,0,0,0])
-        ambient = np.array([0,0,0,0,1,0,0,0])
-        misc = {'freq':5, 'ampl':.2, 'y_offset':1}
-
-    elif config.startswith("sin"):
+    ##############################################
+    # Smooth
+    ##############################################
+    if config.startswith("sin"):
         axis_coord = [0,1]
         shock_pos = 1
         t_end = 1
@@ -57,13 +30,47 @@ def generate_test_conditions(config_variables):
         ambient = np.array([0,1,1,1,1,0,0,0])
         misc = {'peak_pos':0, 'ampl':.75, 'fwhm':.08, 'y_offset':1}
 
-    elif "noh" in config:
+    # [Roy et al., 2004]
+    elif match(any, ["manufacture", "euler"]):
+        axis_coord = [-.5,.5]
+        shock_pos = 0
+        t_end = 2
+        boundary = "wrap"
+        init_cond = np.array([1,.1,.2,.3,1,0,0,0])
+        ambient = np.array([1,.1,.2,.3,1,0,0,0])
+        misc = {'freq':2*np.pi}
+
+    # [Tóth, 2000]
+    elif match(any, ["circular", "polarised", "alfven"]) or config == "cpaw":
+        alpha = np.pi/6
+        axis_coord = [0,1/np.cos(alpha)], [0,1/np.sin(alpha)]
+        shock_pos = 0
+        t_end = 2
+        boundary = "wrap"
+        init_cond = np.array([1,0,0,0,.1,0,0,0])
+        ambient = np.array([1,0,0,0,.1,0,0,0])
+        misc = {'alpha':alpha, 'ampl':.1, 'wave':'moving'}
+
+    elif config.startswith('sq'):
+        axis_coord = [-1,1]
+        shock_pos = 1/3
+        t_end = .05
+        boundary = "wrap"
+        init_cond = np.array([1,1,0,0,1,0,0,0])
+        ambient = np.array([.01,1,0,0,1,0,0,0])
+        misc = None
+
+    ##############################################
+    # Shocktubes
+    ##############################################
+    # [Sod, 1978]
+    elif "sod" in config:
         axis_coord = [0,1]
-        shock_pos = .1
-        t_end = 1
+        shock_pos = .5
+        t_end = .2
         boundary = "edge"
-        init_cond = np.array([1,0,0,0,1e-6,0,0,0])
-        ambient = np.array([16,0,0,0,16/3,0,0,0])
+        init_cond = np.array([1,0,0,0,1,0,0,0])
+        ambient = np.array([.125,0,0,0,.1,0,0,0])
         misc = None
 
     elif "slow" in config:
@@ -75,14 +82,15 @@ def generate_test_conditions(config_variables):
         ambient = np.array([1,-10.5636,0,0,1,0,0,0])
         misc = None
 
-    elif config.startswith('sq'):
+    # [Shu & Osher, 1989]
+    elif match(any, ["shu", "osher"]) or config == "so":
         axis_coord = [-1,1]
-        shock_pos = 1/3
-        t_end = .05
-        boundary = "wrap"
-        init_cond = np.array([1,1,0,0,1,0,0,0])
-        ambient = np.array([.01,1,0,0,1,0,0,0])
-        misc = None
+        shock_pos = -.8
+        t_end = .47
+        boundary = "edge"
+        init_cond = np.array([3.857143,2.629369,0,0,10.3333,0,0,0])
+        ambient = np.array([0,0,0,0,1,0,0,0])
+        misc = {'freq':5, 'ampl':.2, 'y_offset':1}
 
     # [Ryu & Jones, 1995]
     elif match(any, ["ryu", "jones"]) or config == "rj":
@@ -103,192 +111,6 @@ def generate_test_conditions(config_variables):
         init_cond = np.array([1,0,0,0,1,.75,1,0])
         ambient = np.array([.125,0,0,0,.1,.75,-1,0])
         misc = None
-
-    elif match(any, ["kelvin", "helmholtz", "khi"]):
-        axis_coord = [-1,1]
-        shock_pos = .5
-        t_end = 2
-        boundary = "wrap"
-        init_cond = np.array([2,.5,0,0,2.5,0,0,0])
-        ambient = np.array([1,-.5,0,0,2.5,0,0,0])
-        misc = {'perturb':True, 'ampl':.5, 'freq':4, 'Bx':np.sqrt(np.pi)/2}
-
-    elif match(any, ["rayleigh", "taylor", "rti"]):
-        axis_coord = [-.5,.5]
-        shock_pos = 0
-        t_end = 10
-        boundary = "edge"
-        init_cond = np.array([2,.0,0,0,2.5,0,0,0])
-        ambient = np.array([1,0,0,0,2.5,0,0,0])
-        misc = {'perturb':True, 'ampl':.05, 'grav_acc':.1, 'Bx':.05*np.sqrt(np.pi)}
-
-    # [Pang & Wu, 2025]
-    elif match(any, ["ivc", "isentropic"]):
-        axis_coord = [0,10]
-        shock_pos = 5
-        t_end = 10
-        boundary = "wrap"
-        init_cond = np.array([1,0,0,0,1,0,0,0])
-        ambient = np.array([1,0,0,0,1,0,0,0])
-        misc = {'vortex_str':1, 'freq':2}
-
-    # [Roy et al., 2004]
-    elif match(any, ["manufacture", "euler"]):
-        axis_coord = [-.5,.5]
-        shock_pos = 0
-        t_end = 2
-        boundary = "wrap"
-        init_cond = np.array([1,.1,.2,.3,1,0,0,0])
-        ambient = np.array([1,.1,.2,.3,1,0,0,0])
-        misc = {'freq':2*np.pi}
-
-    # [Gresho & Chan, 1990]
-    elif "gresho" in config:
-        axis_coord = [-.5,.5]
-        shock_pos = 0
-        t_end = 1
-        boundary = "wrap"
-        init_cond = np.array([1,0,0,0,0,0,0,0])
-        ambient = np.array([1,0,0,0,0,0,0,0])
-        misc = {'mach':.1}
-
-    # [Orszag & Tang, 1998; Stone et al., 2008; Pang & Wu, 2025]
-    elif match(any, ["orszag", "tang"]) or config == "ot":
-        axis_coord = [0,1]
-        shock_pos = 0
-        t_end = 1
-        boundary = "wrap"
-        init_cond = np.array([gamma**2,0,0,0,gamma,0,0,0])
-        ambient = np.array([gamma**2,0,0,0,gamma,0,0,0])
-        misc = {'norm_factor':2*np.pi, 'ampl':1, 'eps':.2}
-
-    # [Balsara, 2004; Li, 2010]
-    elif match(all, ["mhd", "vortex"]):
-        boundary = "wrap"
-        t_end = 20
-
-        if len(cells) < 3:
-            axis_coord = [-10,10]
-            shock_pos = 10
-            init_cond = np.array([1,0,0,0,1,0,0,0])
-            ambient = np.array([1,0,0,0,1,0,0,0])
-            misc = {'kappa':5, 'mu':5}
-        else:
-            axis_coord = [-5,5]
-            shock_pos = 5
-            init_cond = np.array([1,0,0,2,1,0,0,0])
-            ambient = np.array([1,0,0,2,1,0,0,0])
-            misc = {'kappa':1/np.sqrt(2*np.pi), 'mu':1/np.sqrt(2*np.pi), 'q':1}
-
-    # [Machida et al., 1999]
-    elif "torus" in config:
-        axis_coord = [-7,7]
-        shock_pos = 0
-        t_end = 5
-        boundary = "edge"
-        init_cond = np.array([1e-3,0,0,0,0,0,0,0])
-        ambient = np.array([1e-5,0,0,0,0,0,0,0])
-        misc = {'K':.05, 'B_phi':1, 'GM':1, 'L':1, 'r0':1, 'beta0':1}
-
-    # [Balsara & Spicer, 1999; Pang & Wu, 2025]
-    elif match(any, ["rotor", "blob"]):
-        axis_coord = [-.5,.5]
-        boundary = "wrap"
-        t_end = .5
-        shock_pos = .1
-
-        if "blob" in config:
-            init_cond = np.array([gamma**2,0,0,0,gamma,0,0,0])
-            ambient = np.array([gamma/10,0,0,0,.1,0,0,0])
-            misc = {'beta':1e3, 'eps':1e-6}
-        else:
-            init_cond = np.array([10,0,0,0,.5,2.5,0,0])
-            ambient = np.array([1,0,0,0,.5,2.5,0,0])
-            misc = {'omega':1, 'ring_width':.015}
-
-    # Uniform field with turbulent driving motions [Federrath et al., 2010; Brucy et al., 2024]
-    elif match(any, ["turb", "blank"]):
-        magnetic = True
-        axis_coord = [-.5,.5]
-        shock_pos = 0
-        t_end = 5
-        boundary = "wrap"
-        init_cond = np.array([gamma**2,0,0,0,gamma,0,0,0])
-        ambient = np.array([gamma**2,0,0,0,gamma,0,0,0])
-        misc = {'ampl':.1/np.sqrt(4*np.pi), 'perturb_ampl':.1, 'magnetic':magnetic}
-
-    # [Felker & Stone, 2018]
-    elif match(all, ["mhd", "blast"]):
-        axis_coord = [-.5,.5]
-        shock_pos = .1
-        t_end = .2
-        boundary = "wrap"
-        init_cond = np.array([1,0,0,0,10,0,0,0])
-        ambient = np.array([1,0,0,0,.1,0,0,0])
-        misc = {'ampl':1/np.sqrt(2)}
-
-    # [Gardiner & Stone, 2005]
-    elif match(any, ["current", "sheet"]):
-        axis_coord = [-1,1]
-        shock_pos = .5
-        t_end = 10
-        boundary = "wrap"
-        init_cond = np.array([1,0,0,0,.1,0,1,0])
-        ambient = np.array([1,0,0,0,.1,0,1,0])
-        misc = {'ampl':.1}
-
-    # [Dai & Woodward, 1998; Pang & Wu, 2025]
-    elif "cloud" in config:
-        axis_coord = [0,1]
-        shock_pos = .6
-        t_end = 1
-        boundary = "edge"
-        init_cond = np.array([3.86859,0,0,0,167.345,0,2.1826182,-2.1826182])
-        ambient = np.array([1,-11.2536,0,0,1,0,.56418958,.56418958])
-        misc = {'cloud_mass':10}
-
-    # [Wu & Shu, 2018]
-    elif "jet" in config:
-        axis_coord = [-.5,.5]
-        shock_pos = .05
-        t_end = .01
-        boundary = "edge"
-        init_cond = np.array([gamma*.1,0,0,0,1,0,np.sqrt(20),0])
-        ambient = np.array([gamma*.1,0,0,0,1,0,np.sqrt(20),0])
-        misc = {'perturb':False, 'velocity':800}
-
-    # [Markert et al., 2022]
-    elif match(any, ["supernova", "tycho"]) or config == "sn":
-        # tau0 is the constant in Skumanich's law for stellar rotation
-        # (assumed Sun's age and rotational velocity)
-        rotation = False
-        tau0 = .1708284534  # [(pc/yr)⋅yr^0.51]
-        age = 1e7
-
-        mode = 'quarter'  # quadrant/octant or full-sphere mode
-        shock_pos = 0
-        t_end = 490
-        init_cond = np.array([2.4539e-3,0,0,0,1,0,0,0])
-        ambient = np.array([2.4539e-3,0,0,0,2.1309e-13,0,0,0])
-        misc = {'E':5.2516e-5, 'M':1.4, 't0':10, 'rotation':rotation, 'tau0':tau0, 'age':age, 'mode':mode}
-
-        if mode.lower().startswith(('o','q')):
-            axis_coord = [0,5]
-            boundary = "reflect"
-        else:
-            axis_coord = [-2.5,2.5]
-            boundary = "edge"
-
-    # [Tóth, 2000]
-    elif match(any, ["circular", "polarised", "alfven"]) or config == "cpaw":
-        alpha = np.pi/6
-        axis_coord = [0,1/np.cos(alpha)], [0,1/np.sin(alpha)]
-        shock_pos = 0
-        t_end = 2
-        boundary = "wrap"
-        init_cond = np.array([1,0,0,0,.1,0,0,0])
-        ambient = np.array([1,0,0,0,.1,0,0,0])
-        misc = {'alpha':alpha, 'ampl':.1, 'wave':'moving'}
 
     # [Toro, 1999, p.225]
     elif "toro" in config:
@@ -327,6 +149,211 @@ def generate_test_conditions(config_variables):
             init_cond = np.array([1,.75,0,0,1,0,0,0])
             ambient = np.array([.125,0,0,0,.1,0,0,0])
 
+    ##############################################
+    # Blastwaves
+    ##############################################
+    # [Sedov, 1959]
+    elif "sedov" in config:
+        axis_coord = [-10,10]
+        shock_pos = .5
+        t_end = 2
+        boundary = "wrap"
+        init_cond = np.array([1,0,0,0,100,0,0,0])
+        ambient = np.array([1,0,0,0,1e-12,0,0,0])
+        misc = None
+
+    # [Felker & Stone, 2018]
+    elif match(all, ["mhd", "blast"]):
+        axis_coord = [-.5,.5]
+        shock_pos = .1
+        t_end = .2
+        boundary = "wrap"
+        init_cond = np.array([1,0,0,0,10,0,0,0])
+        ambient = np.array([1,0,0,0,.1,0,0,0])
+        misc = {'ampl':1/np.sqrt(2)}
+
+    elif "noh" in config:
+        axis_coord = [0,1]
+        shock_pos = .1
+        t_end = 1
+        boundary = "edge"
+        init_cond = np.array([1,0,0,0,1e-6,0,0,0])
+        ambient = np.array([16,0,0,0,16/3,0,0,0])
+        misc = None
+
+    ##############################################
+    # Vortices
+    ##############################################
+    # [Gresho & Chan, 1990]
+    elif "gresho" in config:
+        axis_coord = [-.5,.5]
+        shock_pos = 0
+        t_end = 1
+        boundary = "wrap"
+        init_cond = np.array([1,0,0,0,0,0,0,0])
+        ambient = np.array([1,0,0,0,0,0,0,0])
+        misc = {'mach':.1}
+
+    # [Balsara, 2004; Li, 2010]
+    elif match(all, ["mhd", "vortex"]):
+        boundary = "wrap"
+        t_end = 20
+
+        if len(cells) < 3:
+            axis_coord = [-10,10]
+            shock_pos = 10
+            init_cond = np.array([1,0,0,0,1,0,0,0])
+            ambient = np.array([1,0,0,0,1,0,0,0])
+            misc = {'kappa':5, 'mu':5}
+        else:
+            axis_coord = [-5,5]
+            shock_pos = 5
+            init_cond = np.array([1,0,0,2,1,0,0,0])
+            ambient = np.array([1,0,0,2,1,0,0,0])
+            misc = {'kappa':1/np.sqrt(2*np.pi), 'mu':1/np.sqrt(2*np.pi), 'q':1}
+
+    # [Orszag & Tang, 1998; Stone et al., 2008; Pang & Wu, 2025]
+    elif match(any, ["orszag", "tang"]) or config == "ot":
+        axis_coord = [0,1]
+        shock_pos = 0
+        t_end = 1
+        boundary = "wrap"
+        init_cond = np.array([gamma**2,0,0,0,gamma,0,0,0])
+        ambient = np.array([gamma**2,0,0,0,gamma,0,0,0])
+        misc = {'norm_factor':2*np.pi, 'ampl':1, 'eps':.2}
+
+    # [Pang & Wu, 2025]
+    elif match(any, ["ivc", "isentropic"]):
+        axis_coord = [0,10]
+        shock_pos = 5
+        t_end = 10
+        boundary = "wrap"
+        init_cond = np.array([1,0,0,0,1,0,0,0])
+        ambient = np.array([1,0,0,0,1,0,0,0])
+        misc = {'vortex_str':1, 'freq':2}
+
+    ##############################################
+    # Instabilities
+    ##############################################
+    elif match(any, ["kelvin", "helmholtz", "khi"]):
+        axis_coord = [-1,1]
+        shock_pos = .5
+        t_end = 2
+        boundary = "wrap"
+        init_cond = np.array([2,.5,0,0,2.5,0,0,0])
+        ambient = np.array([1,-.5,0,0,2.5,0,0,0])
+        misc = {'perturb':True, 'ampl':.5, 'freq':4, 'Bx':np.sqrt(np.pi)/2}
+
+    elif match(any, ["rayleigh", "taylor", "rti"]):
+        axis_coord = [-.5,.5]
+        shock_pos = 0
+        t_end = 10
+        boundary = "edge"
+        init_cond = np.array([2,.0,0,0,2.5,0,0,0])
+        ambient = np.array([1,0,0,0,2.5,0,0,0])
+        misc = {'perturb':True, 'ampl':.05, 'grav_acc':.1, 'Bx':.05*np.sqrt(np.pi)}
+
+    ##############################################
+    # Turbulent/random noise
+    ##############################################
+    # Uniform field with turbulent driving motions [Federrath et al., 2010; Brucy et al., 2024]
+    elif match(any, ["turb", "blank"]):
+        magnetic = True
+        axis_coord = [-.5,.5]
+        shock_pos = 0
+        t_end = 1
+        boundary = "wrap"
+        init_cond = np.array([gamma**2,0,0,0,gamma,0,0,0])
+        ambient = np.array([gamma**2,0,0,0,gamma,0,0,0])
+        misc = {'ampl':.1/np.sqrt(4*np.pi), 'perturb_ampl':.1, 'magnetic':magnetic}
+
+    ##############################################
+    # Non-linear MHD
+    ##############################################
+    # [Balsara & Spicer, 1999; Pang & Wu, 2025]
+    elif match(any, ["rotor", "blob"]):
+        axis_coord = [-.5,.5]
+        boundary = "wrap"
+        t_end = .5
+        shock_pos = .1
+
+        if "blob" in config:
+            init_cond = np.array([gamma**2,0,0,0,gamma,0,0,0])
+            ambient = np.array([gamma/10,0,0,0,.1,0,0,0])
+            misc = {'beta':1e3, 'eps':1e-6}
+        else:
+            init_cond = np.array([10,0,0,0,.5,2.5,0,0])
+            ambient = np.array([1,0,0,0,.5,2.5,0,0])
+            misc = {'omega':1, 'ring_width':.015}
+
+    # [Gardiner & Stone, 2005]
+    elif match(any, ["current", "sheet"]):
+        axis_coord = [-1,1]
+        shock_pos = .5
+        t_end = 10
+        boundary = "wrap"
+        init_cond = np.array([1,0,0,0,.1,0,1,0])
+        ambient = np.array([1,0,0,0,.1,0,1,0])
+        misc = {'ampl':.1}
+
+    ##############################################
+    # Astrophysical
+    ##############################################
+    # [Dai & Woodward, 1998; Pang & Wu, 2025]
+    elif "cloud" in config:
+        axis_coord = [0,1]
+        shock_pos = .6
+        t_end = 1
+        boundary = "edge"
+        init_cond = np.array([3.86859,0,0,0,167.345,0,2.1826182,-2.1826182])
+        ambient = np.array([1,-11.2536,0,0,1,0,.56418958,.56418958])
+        misc = {'cloud_mass':10}
+
+    # [Wu & Shu, 2018]
+    elif "jet" in config:
+        axis_coord = [-.5,.5]
+        shock_pos = .05
+        t_end = .01
+        boundary = "edge"
+        init_cond = np.array([gamma*.1,0,0,0,1,0,np.sqrt(20),0])
+        ambient = np.array([gamma*.1,0,0,0,1,0,np.sqrt(20),0])
+        misc = {'perturb':False, 'velocity':800}
+
+    # [Machida et al., 1999]
+    elif "torus" in config:
+        axis_coord = [-7,7]
+        shock_pos = 0
+        t_end = 5
+        boundary = "edge"
+        init_cond = np.array([1e-3,0,0,0,0,0,0,0])
+        ambient = np.array([1e-5,0,0,0,0,0,0,0])
+        misc = {'K':.05, 'B_phi':1, 'GM':1, 'L':1, 'r0':1, 'beta0':1}
+
+    # [Markert et al., 2022]
+    elif match(any, ["supernova", "tycho"]) or config == "sn":
+        # tau0 is the constant in Skumanich's law for stellar rotation
+        # (assumed Sun's age and rotational velocity)
+        rotation = False
+        tau0 = .1708284534  # [(pc/yr)⋅yr^0.51]
+        age = 1e7
+
+        mode = 'quarter'  # quadrant/octant or full-sphere mode
+        shock_pos = 0
+        t_end = 490
+        init_cond = np.array([2.4539e-3,0,0,0,1,0,0,0])
+        ambient = np.array([2.4539e-3,0,0,0,2.1309e-13,0,0,0])
+        misc = {'E':5.2516e-5, 'M':1.4, 't0':10, 'rotation':rotation, 'tau0':tau0, 'age':age, 'mode':mode}
+
+        if mode.lower().startswith(('o','q')):
+            axis_coord = [0,5]
+            boundary = "reflect"
+        else:
+            axis_coord = [-2.5,2.5]
+            boundary = "edge"
+
+    ##############################################
+    # 2D Riemann
+    ##############################################
     # [Yee & Sjögreen, 2005]
     elif match(any, ["yee", "sjögreen", "sjoegreen"]) or config == "ys":
         axis_coord = [-1,1]
